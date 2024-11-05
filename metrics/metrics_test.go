25e022c3195f9bf0eb88475954dbe546dbfb7505
Rob Martin <rob@version2beta.com>
2024-11-05 15:48:31 -0600

    fix flaky test

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -71,7 +71,7 @@ func TestMetrics_EmptyAvgDepth(t *testing.T) {
 }
 
 func TestMetrics_Stop(t *testing.T) {
-	m := NewMetrics(20 * time.Microsecond) // Set faster interval for testing
+	m := NewMetrics(200 * time.Microsecond) // Set faster interval for testing
 	defer m.Stop()
 
 	m.AddBits(100)
@@ -80,14 +80,14 @@ func TestMetrics_Stop(t *testing.T) {
 	// Wait until bitRate and avgDepth are updated
 	waitForCondition(t, func() bool {
 		return m.GetBitRate() > 0 && m.GetAvgDepth() > 0
-	}, 500*time.Microsecond)
+	}, 50*time.Millisecond)
 
 	expectedBitRate := m.GetBitRate()
 	expectedAvgDepth := m.GetAvgDepth()
 
 	// Stop the metrics system
 	m.Stop()
-	time.Sleep(20 * time.Microsecond)
+	time.Sleep(50 * time.Microsecond)
 
 	// After stopping, further operations should still be safe
 	m.AddBits(50)
@@ -95,7 +95,7 @@ func TestMetrics_Stop(t *testing.T) {
 
 	// Since the goroutine is stopped, values won't be updated
 	// Wait and check if metrics were updated
-	time.Sleep(20 * time.Microsecond)
+	time.Sleep(50 * time.Millisecond)
 
 	// Check that values before stopping are still accessible
 	assert.Equal(t, expectedBitRate, m.GetBitRate())
